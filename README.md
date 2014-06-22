6c54b049201bd7f2c81e6a118e3c4a28890c8f15
Rajen Kishna <rajenkishna@hotmail.com>
2014-06-22 15:03:35 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-storagehelper
-=============
+# StorageHelper
+
+StorageHelper is a helper class for Windows Phone and Windows apps to easily save, load and delete data from storage. By default, XML (de)serialization is used, but overrides are available for JSON (de)serialization instead.
+
+## How to use
+
+* Download or clone the source code from the GitHub repository or use the [NuGet package] (http://www.nuget.org/packages/StorageHelper/)
+
+### Windows Phone Silverlight
+
+* In Windows Phone Silverlight projects, use the following syntax
+  * NOTE: The persistent storage APIs use the [LocalFolder] (http://msdn.microsoft.com/en-US/library/windows/apps/windows.storage.applicationdata.localfolder.aspx)/[LocalSettings] (http://msdn.microsoft.com/en-US/library/windows/apps/windows.storage.applicationdata.localsettings.aspx) containers for Silverlight apps
+```csharp
+// Use the application state dictionary (temporary storage)
+Storage.SaveState("MYKEY", MYOBJ);
+Storage.LoadState<TYPE>("MYKEY");
+Storage.DeleteState("MYKEY");
+
+// Use the persistent storage (using files)
+[await] Storage.SaveAsync("MYKEY", MYOBJ);
+[await] Storage.SaveAsync("MYKEY", MYOBJ, true); // use JSON
+[await] Storage.LoadAsync<TYPE>("MYKEY");
+[await] Storage.LoadAsync<TYPE>("MYKEY", true); // use JSON
+[await] Storage.DeleteAsync("MYKEY");
+
+// Use the persistent settings storage (using dictionary)
+Storage.SaveSetting("MYKEY", MYOBJ);
+Storage.LoadSetting("MYKEY");
+Storage.DeleteSetting("MYKEY");
+```
+
+### Windows Store (Windows 8.1, Windows Phone 8.1, Universal)
+
+* In Windows Store projects, use the following syntax
+  * NOTE: The storage APIs use the [RoamingFolder] (http://msdn.microsoft.com/en-US/library/windows/apps/windows.storage.applicationdata.roamingfolder.aspx)/[RoamingSettings] (http://msdn.microsoft.com/en-US/library/windows/apps/windows.storage.applicationdata.roamingsettings.aspx) containers for Windows Store apps, but overrides are available to use their local counterparts instead.
+```csharp
+// Use the persistent storage (using files)
+[await] Storage.SaveAsync("MYKEY", MYOBJ);
+[await] Storage.SaveAsync("MYKEY", MYOBJ, true); // use JSON
+[await] Storage.SaveAsync(ApplicationData.Current.LocalFolder, "MYKEY", MYOBJ);
+[await] Storage.LoadAsync<TYPE>("MYKEY");
+[await] Storage.LoadAsync<TYPE>("MYKEY", true); // use JSON
+[await] Storage.LoadAsync<TYPE>(ApplicationData.Current.LocalFolder, "MYKEY");
+[await] Storage.DeleteAsync("MYKEY");
+[await] Storage.DeleteAsync(ApplicationData.Current.LocalFolder, "MYKEY");
+
+// Use the persistent settings storage (using dictionary)
+Storage.SaveSetting("MYKEY", MYOBJ);
+Storage.SaveSetting(ApplicationData.Current.LocalSettings, "MYKEY", MYOBJ);
+Storage.LoadSetting("MYKEY");
+Storage.LoadSetting(ApplicationData.Current.LocalSettings, "MYKEY");
+Storage.DeleteSetting("MYKEY");
+Storage.DeleteSetting(ApplicationData.Current.LocalSettings, "MYKEY");
+```
+
+## Credits
+
+This library has been written by [Rajen Kishna] (https://twitter.com/rajen_k) with input from [Dave Smits] (https://twitter.com/davesmits).
+This library has been created as a side-project to assist the community and is provided "as is" with no warranty whatsoever and has no relations to our employers (Microsoft and Sparked).
+
---
+++ StorageHelper/StorageHelper.WindowsPhoneSilverlight/Storage.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Windows.Storage;
+
+#if WINDOWS_PHONE
+using Microsoft.Phone.Shell;
+
+namespace StorageHelper.WindowsPhoneSilverlight
+#elif WINDOWS_PHONE_APP
+namespace StorageHelper.WindowsStore
+#elif WINDOWS_APP
+namespace StorageHelper.WindowsStore
+#elif NETFX_CORE
+namespace StorageHelper.WindowsStore
+#endif
+{
+    public class Storage
+    {
+#if WINDOWS_PHONE
+        private static StorageFolder DefaultFolder = ApplicationData.Current.LocalFolder;
+        private static ApplicationDataContainer DefaultSettings = ApplicationData.Current.LocalSettings;
+#elif NETFX_CORE
+        private static StorageFolder DefaultFolder = ApplicationData.Current.RoamingFolder;
+        private static ApplicationDataContainer DefaultSettings = ApplicationData.Current.RoamingSettings;
+#endif
+
+#if WINDOWS_PHONE
+        #region Save State
+        /// <summary>
+        /// Saves an element with the provided key and value to the application's state.
+        /// </summary>
+        /// <param name="key">The key of the element to save.</param>
+        /// <param name="value">The object to save.</param>
+        /// <returns>True if the element has been saved.</returns>
+        public static bool SaveState(string key, object value)
+        {
+            if (null == value)
+                return false;
+
+            if (PhoneApplicationService.Current.State.ContainsKey(key))
+                PhoneApplicationService.Current.State.Remove(key);
+
+            PhoneApplicationService.Current.State.Add(key, value);
+            return true;
+        }
+        #endregion
+
+        #region Load State
+        /// <summary>
+        /// Gets the value associated with the specified key from the application's state.
+        /// </summary>
+        /// <typeparam name="T">The type of the object at the specified key.</typeparam>
+        /// <param name="key">The key of the element to retrieve.</param>
+        /// <returns>The retrieved element or an empty object of type T if no element was found at the specified key.</returns>
+        public static T LoadState<T>(string key)
+        {
+            object result;
+
+            if (!PhoneApplicationService.Current.State.TryGetValue(key, out result))
+                result = default(T);
+
+            return (T)result;
+        }
+        #endregion
+
+        #region Delete State
+        /// <summary>
+        /// Removes the element with the specified key from the application's state.
+        /// </summary>
+        /// <param name="key">The key of the element to remove.</param>
+        /// <returns>True if the key has been removed or did not exist.</returns>
+        public static bool DeleteState(string key)
+        {
+            if (null == key)
+                return false;
+
+            if (PhoneApplicationService.Current.State.ContainsKey(key))
+                PhoneApplicationService.Current.State.Remove(key);
+            return true;
+        }
+        #endregion
+#endif
+
+        #region Save Storage
+        /// <summary>
+#if WINDOWS_PHONE
+        /// Saves an entry with the provided path and value to the application's persisted local storage.
+#elif NETFX_CORE
+        /// Saves an entry with the provided path and value to the application's persisted roaming storage.
+#endif
+        /// </summary>
+        /// <param name="path">The path for the entry to save.</param>
+        /// <param name="data">The object to save.</param>
+        /// <param name="useJson">Optional parameter to use JSON instead of XML</param>
+        /// <returns>True if the entry has been saved.</returns>
+        public static Task<bool> SaveAsync(string path, object data, bool useJson = false)
+        {
+            return SaveAsync(DefaultFolder, path, data, useJson);
+        }
+
+        /// <summary>
+        /// Saves an entry with the provided path and value to the application's persisted specified storage.
+        /// </summary>
+        /// <param name="folder">The folder to save the entry to.</param>
+        /// <param name="path">The path for the entry to save.</param>
+        /// <param name="data">The object to save.</param>
+        /// <param name="useJson">Optional parameter to use JSON instead of XML</param>
+        /// <returns>True if the entry has been saved.</returns>
+        public async static Task<bool> SaveAsync(StorageFolder folder, string path, object data, bool useJson = false)
+        {
+            const CreationCollisionOption option = CreationCollisionOption.ReplaceExisting;
+
+            try
+            {
+                var file = await folder.CreateFileAsync(path, option).AsTask().ConfigureAwait(false);
+                var saveData = new MemoryStream();
+
+                if (!useJson)
+                {
+                    var x = new XmlSerializer(data.GetType());
+                    x.Serialize(saveData, data);
+                }
+                else
+                {
+                    var j = new DataContractJsonSerializer(data.GetType());
+                    j.WriteObject(saveData, data);
+                }
+
+                if (saveData.Length > 0)
+                {
+                    using (var fileStream = await file.OpenStreamForWriteAsync().ConfigureAwait(false))
+                    {
+                        saveData.Seek(0, SeekOrigin.Begin);
+                        await saveData.CopyToAsync(fileStream);
+                        await fileStream.FlushAsync();
+                    }
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Load Storage
+        /// <summary>
+#if WINDOWS_PHONE
+        /// Gets the value associated with the specified path from the application's persisted local storage.
+#elif NETFX_CORE
+        /// Gets the value associated with the specified path from the application's persisted roaming storage.
+#endif
+        /// </summary>
+        /// <typeparam name="T">The type of the object at the specified path.</typeparam>
+        /// <param name="path">The path for the entry to retrieve.</param>
+        /// <param name="useJson">Optional parameter to use JSON instead of XML.</param>
+        /// <returns>The retrieved element or an empty object of type T if no entry was found at the specified path.</returns>
+        public static Task<T> LoadAsync<T>(string path, bool useJson = false)
+        {
+            return LoadAsync<T>(DefaultFolder, path, useJson);
+        }
+
+        /// <summary>
+        /// Gets the value associated with the specified path from the application's persisted specified storage.
+        /// </summary>
+        /// <typeparam name="T">The type of the object at the specified path.</typeparam>
+        /// <param name="folder">The folder to retrieve the entry from.</param>
+        /// <param name="path">The path for the entry to retrieve.</param>
+        /// <param name="useJson">Optional parameter to use JSON instead of XML.</param>
+        /// <returns>The retrieved element or an empty object of type T if no entry was found at the specified path.</returns>
+        public async static Task<T> LoadAsync<T>(StorageFolder folder, string path, bool useJson = false)
+        {
+            try
+            {
+                var file = await folder.GetFileAsync(path).AsTask().ConfigureAwait(false);
+
+                using (var inStream = await file.OpenSequentialReadAsync().AsTask().ConfigureAwait(false))
+                {
+                    if (!useJson)
+                    {
+                        var x = new XmlSerializer(typeof(T));
+                        return (T)x.Deserialize(inStream.AsStreamForRead());
+                    }
+                    else
+                    {
+                        var j = new DataContractJsonSerializer(typeof(T));
+                        return (T)j.ReadObject(inStream.AsStreamForRead());
+                    }
+                }
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+        #endregion
+
+        #region Delete Storage
+        /// <summary>
+#if WINDOWS_PHONE
+        /// Removes the entry with the specified path from the application's persisted local storage.
+#elif NETFX_CORE
+        /// Removes the entry with the specified path from the application's persisted roaming storage.
+#endif
+        /// </summary>
+        /// <param name="path">The path for the entry to remove.</param>
+        /// <returns>True if the entry has been removed.</returns>
+        public static Task<bool> DeleteAsync(string path)
+        {
+            return DeleteAsync(DefaultFolder, path);
+        }
+
+        /// <summary>
+        /// Removes the entry with the specified path from the application's persisted specified storage.
+        /// </summary>
+        /// <param name="folder">The folder to remove the entry from.</param>
+        /// <param name="path">The path for the entry to remove.</param>
+        /// <returns>True if the entry has been removed.</returns>
+        public async static Task<bool> DeleteAsync(StorageFolder folder, string path)
+        {
+            try
+            {
+                var file = await folder.GetFileAsync(path).AsTask().ConfigureAwait(false);
+                await file.DeleteAsync().AsTask().ConfigureAwait(false);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Save Setting
+        /// <summary>
+#if WINDOWS_PHONE
+        /// Saves an entry with the provided key and value to the application's persisted local settings.
+#elif NETFX_CORE
+        /// Saves an entry with the provided key and value to the application's persisted roaming settings.
+#endif
+        /// </summary>
+        /// <param name="key">The key for the entry to save.</param>
+        /// <param name="value">The object to save.</param>
+        /// <returns>True if the entry has been saved.</returns>
+        public static bool SaveSetting(string key, object value)
+        {
+            return SaveSetting(DefaultSettings, key, value);
+        }
+
+        /// <summary>
+        /// Saves an entry with the provided key and value to the application's persisted specified settings store.
+        /// </summary>
+        /// <param name="container">The container to save the entry to.</param>
+        /// <param name="key">The key for the entry to save.</param>
+        /// <param name="value">The object to save.</param>
+        /// <returns>True if the entry has been saved.</returns>
+        public static bool SaveSetting(ApplicationDataContainer container, string key, object value)
+        {
+            try
+            {
+                container.Values[key] = value;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Load Setting
+        /// <summary>
+#if WINDOWS_PHONE
+        /// Gets the value associated with the specified key from the application's persisted local settings.
+#elif NETFX_CORE
+        /// Gets the value associated with the specified key from the application's persisted roaming settings.
+#endif
+        /// </summary>
+        /// <typeparam name="T">The type of the object at the specified key.</typeparam>
+        /// <param name="key">The key for the entry to retrieve.</param>
+        /// <returns>The retrieved element or an empty object of type T if no entry was found at the specified key.</returns>
+        public static T LoadSetting<T>(string key)
+        {
+            return LoadSetting<T>(DefaultSettings, key);
+        }
+
+        /// <summary>
+        /// Gets the value associated with the specified key from the application's persisted specified settings store.
+        /// </summary>
+        /// <typeparam name="T">The type of the object at the specified key.</typeparam>
+        /// <param name="container">The container to retrieve the entry from.</param>
+        /// <param name="key">The key for the entry to retrieve.</param>
+        /// <returns>The retrieved element or an empty object of type T if no entry was found at the specified key.</returns>
+        private static T LoadSetting<T>(ApplicationDataContainer container, string key)
+        {
+            try
+            {
+                var value = container.Values[key];
+                if (value == null)
+                    return default(T);
+                return (T)value;
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+        #endregion
+
+        #region Delete Setting
+        /// <summary>
+#if WINDOWS_PHONE
+        /// Removes the entry with the specified key from the application's persisted local settings.
+#elif NETFX_CORE
+        /// Removes the entry with the specified key from the application's persisted roaming settings.
+#endif
+        /// </summary>
+        /// <param name="key">The key for the entry to remove.</param>
+        /// <returns>True if the entry has been removed.</returns>
+        public static bool DeleteSetting(string key)
+        {
+            return DeleteSetting(DefaultSettings, key);
+        }
+
+        /// <summary>
+        /// Removes the entry with the specified key from the application's persisted specified settings store.
+        /// </summary>
+        /// <param name="container">The container to remove the entry from.</param>
+        /// <param name="key">The key for the entry to remove.</param>
+        /// <returns>True if the entry has been removed.</returns>
+        private static bool DeleteSetting(ApplicationDataContainer container, string key)
+        {
+            try
+            {
+                container.Values.Remove(key);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
